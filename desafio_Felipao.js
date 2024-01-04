6206481a058fa58645aee62b18343e6afeb6ac0a
Fabio Santos <56350137+FabioKOF@users.noreply.github.com>
2024-01-04 13:17:34 -0300

    Add files via upload

---
+++ desafio_Felipao.js
@@ -0,0 +1,41 @@
+
+let level = ""
+let nome = ""
+
+switch(level){
+	case "10001":
+    	console.log("Seu nível é: radiante")
+        break;
+        
+  	case "9001":
+    	console.log("Seu nível é: imortal")
+        break;
+
+	case "8001":
+		console.log("Seu nível é: ascendente")
+        break;
+	
+    case "7001":
+    	console.log("Seu nível é: platina")
+        break;
+
+    case "5001":
+        console.log("Seu nível é: ouro")
+        break;
+
+    case "2001":
+        console.log("Seu nível é: prata")
+        break;
+
+    case "1001":
+        console.log("Seu nível é: bronze")
+        break;
+
+    default:
+        console.log("Seu nível é: ferro)
+    }
+
+    console.log(`Seu herói chamado ${nome} tem o nível ${level} tenha uma boa aventura.`)
+
+
+
